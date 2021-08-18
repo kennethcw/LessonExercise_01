a2ea18c7d69f8cf297dba5077f4c29b3614f83ae
Kenneth Chew <72385483+kennethcw@users.noreply.github.com>
2021-08-18 15:33:06 +0200

    Added News

---
+++ LessonExercise_01/Pages/News.cshtml.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace LessonExercise_01.Pages
+{
+    public class NewsModel : PageModel
+    {
+        public List<NewsItemList> Items { get; set; }
+
+        public class NewsItemList
+        {
+            public string Title { get; set; }
+            public string Desc { get; set; }
+        }
+        public void OnGet()
+        {
+            Items = new List<NewsItemList>
+            {
+                new NewsItemList{Desc = "Joe Biden hits the dab", Title = "Joe Biden did what?"},
+                new NewsItemList{Desc = "F1 fast", Title = "Car"},
+                new NewsItemList{Desc = "Hehe", Title = "Haha"}
+            };
+        }   
+    }
+}
