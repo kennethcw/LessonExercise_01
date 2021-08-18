e8795ca0a5340ee0aa9b7b4ab19eea8612d0e041
Kenneth Chew <72385483+kennethcw@users.noreply.github.com>
2021-08-18 14:29:56 +0200

    Translated weekday to swedish

--- LessonExercise_01/Pages/Privacy.cshtml.cs
+++ LessonExercise_01/Pages/Privacy.cshtml.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -12,6 +13,7 @@ namespace LessonExercise_01.Pages
     {
         private readonly ILogger<PrivacyModel> _logger;
 
+        public string DayOfTheWeekInSwedish { get; set; }
         public PrivacyModel(ILogger<PrivacyModel> logger)
         {
             _logger = logger;
@@ -19,6 +21,10 @@ namespace LessonExercise_01.Pages
 
         public void OnGet()
         {
+            DayOfTheWeekInSwedish = DateTime.Now.ToString();
+
+            var svenska = new CultureInfo("sv-SE");
+            DayOfTheWeekInSwedish = DateTime.Now.ToString("dddd", svenska);
         }
     }
 }
